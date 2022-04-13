9fb407450c16120a7d8b2dd51e21e3a1170f5ef5
Othmane Hanyf <othmane.hanyf@protonmail.com>
2022-04-13 17:37:03 +0000

    fixing issue

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 import homeHero from "./home_hero.png";
 import homeCards from "./home_cards.png";
-import feature from "./feature.png";
 import code from "./code.png";
 import postgresql from "./postgresql.svg";
 import laravel from "./laravel.svg";
@@ -9,7 +8,6 @@ import htmlCssJs from "./htmlCssJs.png";
 export default {
   homeHero,
   homeCards,
-  feature,
   code,
   htmlCssJs,
   postgresql,
